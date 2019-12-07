949930ea092727b5615c684a639f463c342542c2
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2019-12-07 17:06:18 -0300

    Add files via upload

---
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,15 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        
+        Date relogio = new Date();
+        
+        System.out.println("Hora atual do sistema");
+        System.out.println(relogio.toString());
+    }
+    
+}
